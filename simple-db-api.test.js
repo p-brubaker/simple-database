bc38cc47cb9903a66c8e8d28f49c1b8364411c70
Paul Brubaker <pbrubak@gmail.com>
2021-09-15 23:00:37 -0700

    save and get test passing

--- simple-db-api.test.js
+++ simple-db-api.test.js
@@ -1,37 +1,36 @@
-import { rm, mkdir } from 'fs/promises';
+import { rmdirSync, mkdirSync } from 'fs';
 import { SimpleDb } from './simple-db-api.js';
 
 describe('simple database', () => {
-    const rootDir = './__test__rootdir';
+    const rootDir = '__test__rootdir';
 
     beforeEach(() => {
-        return rm(rootDir, { force: true, recursive: true })
-            .then(mkdir(rootDir))
-            .catch((err) => console.error(err));
+        rmdirSync(rootDir, { recursive: true, force: true });
+        mkdirSync(rootDir);
     });
 
-    it.only('should return an id when an object is saved', () => {
+    it('should return an id when an object is saved', () => {
         const simpleDb = new SimpleDb(rootDir);
-        const fakeId = simpleDb.save({ data: 'fake data' });
-        expect(fakeId).toEqual(expect.any(String));
+        return simpleDb.save({ data: 'fake data' }).then((id) => {
+            expect(id).toEqual(expect.any(String));
+        });
     });
 
     it('should save and then get an object', () => {
         const simpleDb = new SimpleDb(rootDir);
         const fakeData = { data: 'fake data' };
-        const dataGotten = simpleDb
-            .save(fakeData)
-            .then((id) => {
-                simpleDb.get(id);
-            })
-            .catch((err) => console.error(err));
-        expect(dataGotten).toStrictEqual({
-            id: expect.any(String),
-            data: 'fake data',
+
+        return simpleDb.save(fakeData).then((id) => {
+            return simpleDb.get(id).then((result) => {
+                expect(result).toStrictEqual({
+                    id: expect.any(String),
+                    data: 'fake data',
+                });
+            });
         });
     });
 
-    it('should return null for a non-existant id', () => {
+    it.skip('should return null for a non-existant id', () => {
         const simpleDb = new SimpleDb(rootDir);
         const noIdResult = simpleDb
             .get('thisIdDoesNotExist')
@@ -39,7 +38,7 @@ describe('simple database', () => {
         expect(noIdResult).toEqual(null);
     });
 
-    it('should return all the objects in the root directory', () => {
+    it.skip('should return all the objects in the root directory', () => {
         const simpleDb = new SimpleDb(rootDir);
         const allObjects = [
             { data: 'fake1' },
@@ -57,7 +56,7 @@ describe('simple database', () => {
         ]);
     });
 
-    it('should remove an object', () => {
+    it.skip('should remove an object', () => {
         const simpleDb = new SimpleDb(rootDir);
         const fakeObject = { data: 'fake' };
         const shouldBeNull = simpleDb.save(fakeObject).then((id) => {
@@ -75,7 +74,7 @@ describe('simple database', () => {
         //use get with the id again to check the object really got deleted
     });
 
-    it('should update an object', () => {
+    it.skip('should update an object', () => {
         const simpleDb = new SimpleDb(rootDir);
         const fakeObject = { data: 'fake' };
         const objToUpDateWith = { data: 'updated fake' };
